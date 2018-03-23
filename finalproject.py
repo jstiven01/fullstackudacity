6bba65543e1989f0950897e54f46281ff4b33e14
jstiven01 <stiven10@gmail.com>
2018-03-23 12:01:53 -0500

    Creating html about restaurants (new/edit/delete) Fake restaurants Adding flash messaging and superkey with random

--- finalproject.py
+++ finalproject.py
@@ -1,26 +1,42 @@
-from flask import Flask
+from flask import Flask,render_template, flash
+from os import urandom #Secret key
 app = Flask(__name__)
 
+#Dictionaries for testing
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+#####
 ## Show all restaurants
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
-	return "This page will show all restaurants"
+	return render_template('restaurants.html',restaurants = restaurants)
 
 #Create new restaurant
 @app.route('/restaurant/new')
 def newRestaurant():
-	return "This page will allow to create a new restaurant"
+    flash("New Restaurant Created!")
+    return render_template('newrestaurant.html')
 
 #Edit restaurant
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-	return "This page will edit the restaurant %s" % restaurant_id
+    flash("Restaurant Edited!")
+    return render_template('editrestaurant.html', restaurant_name = restaurant["name"])
 
 #Delete restaurant
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-	return "This page will delete the restaurant %s" % restaurant_id
+    flash("Restaurant Deleted!")
+    return render_template('deleterestaurant.html', restaurant_name=restaurant["name"])
+
 
 #Show Restaurant's menu
 @app.route('/restaurant/<int:restaurant_id>/menu')
@@ -44,5 +60,6 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host = '0.0.0.0', port=8000)
+    app.secret_key = urandom(24)
+    app.debug = True
+    app.run(host = '0.0.0.0', port=8000)
