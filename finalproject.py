6c8433a6a951725f39e8b491d0af458ff61a04e1
jstiven01 <stiven10@gmail.com>
2018-03-24 07:25:39 -0500

    Creating html about Menu (new/edit/delete) Modifying functions which support Menu's (new/edit/delete)

--- finalproject.py
+++ finalproject.py
@@ -41,22 +41,25 @@ def deleteRestaurant(restaurant_id):
 #Show Restaurant's menu
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-	return "This page will show the menu of restaurant %s" % restaurant_id
+    return render_template('menu.html', restaurant_name=restaurant["name"], items = items)
 
 #Create new Menu's item
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-	return "This page will allow to create a new Menu's item of restaurant %s" % restaurant_id
+    flash("New Menu Item Created!")
+    return render_template('newmenuitem.html', restaurant_name=restaurant["name"])
 
 #Edit Menu's item
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-	return "This page will edit Menu's item %s of restaurant %s" % (menu_id, restaurant_id)
+    flash("Menu Item Edited!")
+    return render_template('editmenuitem.html', restaurant_name=restaurant["name"], item = item)
 
 #Delete Menu's item
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-	return "This page will delete Menu's item %s of restaurant %s" % (menu_id, restaurant_id,)
+    flash("Menu Item Deleted!")
+    return render_template('deletemenuitem.html', restaurant_name=restaurant["name"], item_name = item["name"])
 
 
 if __name__ == '__main__':
